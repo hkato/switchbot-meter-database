7aa8a6ac2871e54bd50130a1645b485499be40e8
Hideyuki KATO <hideyuki@kato.jp>
2024-06-23 17:22:38 +0900

    Update: 各種センサーデバイスに対応し照度計にも対応、ライブラリーとしてpython-switchbotを使用、機微情報のイメージへの埋め込みを廃止
    - 温湿度計(Meter)、防水温湿度計(WoIOSensor)、加湿器(Humidifier)、ハブ2(Hub 2)にも対応
    - 照度(lightLevel)に対応(現在の対応デバイスはHub 2のみ)
    - 機微情報(.envのトークンやデバイスIDの含まれたJSONファイル)をイメージに埋め込むのを廃止(dotenvを削除)
    - PyPIで公開されているpython-switchbotライブラリーを使用する方法に変更(switchbot.pyは削除)
    - typing-extensions関連がpython-switchbotとinflux-clientで互換性がないので他のライブラリーも含めてアップデート
    - フルパッケージは不要なのでコンテナイメージを小さくするためにslimイメージを利用

--- app/main.py
+++ app/main.py
@@ -1,74 +1,94 @@
-import json
 import logging
 import os
 from time import sleep
+from typing import List
 
 import schedule
-from dotenv import load_dotenv
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
-from switchbot import Switchbot
+from switchbot import SwitchBot
+from switchbot.devices import Device
 
 # Logging
 formatter = "[%(levelname)-8s] %(asctime)s %(funcName)s %(message)s"
 logging.basicConfig(level=logging.INFO, format=formatter)
 logger = logging.getLogger(__name__)
 
-load_dotenv(".env")
-
 # InfluxDB
-INFLUXDB_TOKEN = os.environ["INFLUXDB_TOKEN"]
-bucket = "switchbot"
-client = InfluxDBClient(url="http://influxdb:8086", token=INFLUXDB_TOKEN, org="org")
+url = os.environ["INFLUXDB_URL"]
+token = os.environ["INFLUXDB_TOKEN"]
+org = os.environ["INFLUXDB_ORG"]
+bucket = os.environ["INFLUXDB_BUCKET"]
+client = InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 query_api = client.query_api()
 
 # SwitchBot
 ACCESS_TOKEN: str = os.environ["SWITCHBOT_ACCESS_TOKEN"]
 SECRET: str = os.environ["SWITCHBOT_SECRET"]
+SUPPORTED_DEVICES = ["Meter", "MeterPlus", "WoIOSensor", "Humidifier", "Hub 2"]
+LIGHT_LEVEL_SUPPORTED_DEVICES = ["Hub 2"]
 
 
-def save_device_status(status: dict):
-    """SwitchbotデバイスのステータスをInfluxDBに保存する"""
+class SwitchBotMeter(SwitchBot):
+    """SwitchBotのうち環境センサー(temperature/humidity/lightLevel)をサポートするデバイスの継承クラス"""
 
-    device_type = status.get("deviceType")
-
-    if device_type == "MeterPlus":
-        p = (
-            Point("MeterPlus")
-            .tag("device_id", status["deviceId"])
-            .field("humidity", float(status["humidity"]))
-            .field("temperature", float(status["temperature"]))
-        )
-
-        write_api.write(bucket=bucket, record=p)
-        logging.info(f"Saved: {status}")
+    def devices(self) -> List[Device]:
+        """環境センサーデバイス一覧のみを返す"""
+        response = self.client.get("devices")
+        return [
+            Device.create(client=self.client, id=device["device_id"], **device)
+            for device in response["body"]["device_list"]
+            if device["device_type"] in SUPPORTED_DEVICES
+        ]
 
 
-def task():
+def get_meter_devices() -> dict:
+    """環境センサーを持つデバイスの一覧を取得"""
+    switchbot = SwitchBotMeter(token=ACCESS_TOKEN, secret=SECRET)
+
+    meter_devices = {}
+    for device in switchbot.devices():
+        meter_devices[device.id] = device.type
+
+    logger.info(f"Meter devices: {meter_devices}")
+
+    return meter_devices
+
+
+def task(devices):
     """定期実行するタスク"""
-    bot = Switchbot(ACCESS_TOKEN, SECRET)
+    switchbot = SwitchBotMeter(token=ACCESS_TOKEN, secret=SECRET)
 
-    with open("device_list.json", "r") as f:
-        device_list = json.load(f)
+    for device_id in devices.keys():
+        device = switchbot.device(device_id)
+        try:
+            status = device.status()
+        except Exception as e:
+            logging.error(f"Request error: {e}")
+            continue
 
-    for d in device_list:
-        device_type = d.get("deviceType")
-        if device_type == "MeterPlus":
-            try:
-                status = bot.get_device_status(d.get("deviceId"))
-            except Exception as e:
-                logging.error(f"Request error: {e}")
-                continue
+        try:
+            p = (
+                Point(device.type)
+                .tag("device_id", status["device_id"])
+                .field("humidity", float(status["humidity"]))
+                .field("temperature", float(status["temperature"]))
+            )
 
-            try:
-                save_device_status(status)
-            except Exception as e:
-                logging.error(f"Save error: {e}")
+            if device.type in LIGHT_LEVEL_SUPPORTED_DEVICES:
+                p.field("light_level", int(status["light_level"]))
+
+            write_api.write(bucket=bucket, record=p)
+            logging.info(f"Saved: {status}")
+        except Exception as e:
+            logging.error(f"Save error: {e}")
 
 
 if __name__ == "__main__":
-    schedule.every(5).minutes.do(task)
+    devices = get_meter_devices()
+
+    schedule.every(5).minutes.do(task, devices)
 
     while True:
         schedule.run_pending()
