73d47bdc48cc486e5f7759a87a74de431b47a205
Hideyuki KATO <hideyuki@kato.jp>
2024-07-03 19:07:35 +0900

    Add CLI mode

--- switchbot_influxdb/main.py
+++ switchbot_influxdb/main.py
@@ -1,3 +1,7 @@
+"""Write SwitchBot environmental sensor data to InfluxDB"""
+
+import argparse
+import dataclasses
 import logging
 import os
 from time import sleep
@@ -14,20 +18,27 @@ formatter = "[%(levelname)-8s] %(asctime)s %(funcName)s %(message)s"
 logging.basicConfig(level=logging.INFO, format=formatter)
 logger = logging.getLogger(__name__)
 
-# InfluxDB
-url = os.environ["INFLUXDB_URL"]
-token = os.environ["INFLUXDB_TOKEN"]
-org = os.environ["INFLUXDB_ORG"]
-bucket = os.environ["INFLUXDB_BUCKET"]
-client = InfluxDBClient(url=url, token=token, org=org)
-write_api = client.write_api(write_options=SYNCHRONOUS)
-query_api = client.query_api()
-
 # SwitchBot
-ACCESS_TOKEN: str = os.environ["SWITCHBOT_ACCESS_TOKEN"]
-SECRET: str = os.environ["SWITCHBOT_SECRET"]
 SUPPORTED_DEVICES = ["Meter", "MeterPlus", "WoIOSensor", "Humidifier", "Hub 2"]
 LIGHT_LEVEL_SUPPORTED_DEVICES = ["Hub 2"]
+
+
+@dataclasses.dataclass
+class SwitchBotAccess:
+    """SwitchBot access data"""
+
+    token: str
+    secret: str
+
+
+@dataclasses.dataclass
+class InfluxDBAccess:
+    """InfluxDB access data"""
+
+    url: str
+    token: str
+    org: str
+    bucket: str
 
 
 class SwitchBotMeter(SwitchBot):
@@ -43,24 +54,11 @@ class SwitchBotMeter(SwitchBot):
         ]
 
 
-def get_meter_devices() -> dict:
-    """環境センサーを持つデバイスの一覧を取得"""
-    switchbot = SwitchBotMeter(token=ACCESS_TOKEN, secret=SECRET)
-
-    meter_devices = {}
-    for device in switchbot.devices():
-        meter_devices[device.id] = device.type
-
-    logger.info(f"Meter devices: {meter_devices}")
-
-    return meter_devices
-
-
-def task(devices):
+def task(influxdb_access, switchbot_access, meter_devices):
     """定期実行するタスク"""
-    switchbot = SwitchBotMeter(token=ACCESS_TOKEN, secret=SECRET)
+    switchbot = SwitchBotMeter(token=switchbot_access.token, secret=switchbot_access.secret)
 
-    for device_id in devices.keys():
+    for device_id in meter_devices.keys():
         device = switchbot.device(device_id)
         try:
             status = device.status()
@@ -79,17 +77,47 @@ def task(devices):
             if device.type in LIGHT_LEVEL_SUPPORTED_DEVICES:
                 p.field("light_level", int(status["light_level"]))
 
-            write_api.write(bucket=bucket, record=p)
+            client = InfluxDBClient(url=influxdb_access.url, token=influxdb_access.token, org=influxdb_access.org)
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            write_api.write(bucket=influxdb_access.bucket, record=p)
             logging.info(f"Saved: {status}")
         except Exception as e:
             logging.error(f"Save error: {e}")
 
 
+def main():
+    """CLI main"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--daemon", action="store_true", help="Daemon mode")
+    parser.add_argument("-t", "--time", default=5, help="Time interval", type=int)
+    parser.add_argument("--url", default=os.getenv("INFLUXDB_URL"), help="InfluxDB URL")
+    parser.add_argument("--token", default=os.getenv("INFLUXDB_TOKEN"), help="InfluxDB token")
+    parser.add_argument("--org", default=os.getenv("INFLUXDB_ORG"), help="InfluxDB organization")
+    parser.add_argument("--bucket", default=os.getenv("INFLUXDB_BUCKET"), help="InfluxDB bucket")
+    parser.add_argument("--switchbot-token", default=os.getenv("SWITCHBOT_TOKEN"), help="SwitchBot token")
+    parser.add_argument("--switchbot-secret", default=os.getenv("SWITCHBOT_SECRET"), help="SwitchBot secret")
+    args = parser.parse_args()
+
+    influxdb_access = InfluxDBAccess(args.url, args.token, args.org, args.bucket)
+    switchbot_access = SwitchBotAccess(args.switchbot_token, args.switchbot_secret)
+
+    logger.info("Start")
+
+    switchbot = SwitchBotMeter(token=switchbot_access.token, secret=switchbot_access.secret)
+
+    meter_devices = {}
+    for device in switchbot.devices():
+        meter_devices[device.id] = device.type
+
+    logger.info(f"Meter devices: {meter_devices}")
+
+    task(influxdb_access, switchbot_access, meter_devices)
+    if args.daemon:
+        schedule.every(args.time).minutes.do(task, influxdb_access, switchbot_access, meter_devices)
+        while True:
+            schedule.run_pending()
+            sleep(1)
+
+
 if __name__ == "__main__":
-    devices = get_meter_devices()
-
-    schedule.every(5).minutes.do(task, devices)
-
-    while True:
-        schedule.run_pending()
-        sleep(1)
+    main()
